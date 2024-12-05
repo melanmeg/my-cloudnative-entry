3b0a6ab56ada93cc035e8e012a15f12b0acba316
melanmeg <naoya05280708@gmail.com>
2024-12-06 03:06:06 +0900

    fix

---
+++ test_terraform/README.md
@@ -0,0 +1,10 @@
+## Terraform
+```bash
+$ terraform providers lock -platform=linux/amd64
+```
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply --auto-approve
+```
---
+++ test_terraform/main.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "example_bucket" {
+  name          = "test-project-373118-sample-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+}
